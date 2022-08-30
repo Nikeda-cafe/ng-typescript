b92348e69883346892551cfb3e8f8a8f91fd12ee
nikeda <4104cafeaulait@gmail.com>
2022-08-31 01:00:25 +0900

    20220831

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HogeComponent } from './hoge/hoge.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'hoge', component: HogeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
